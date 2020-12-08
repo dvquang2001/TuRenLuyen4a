ff1eaedbe5caba32a878ffba1d0461fc0d511b23
Your Name <you@example.com>
2020-12-08 23:27:56 +0700

    thay doi lop SanPham

--- BaiTuRenLuyen4a/app/src/main/java/com/example/model/SanPham.java
+++ BaiTuRenLuyen4a/app/src/main/java/com/example/model/SanPham.java
@@ -4,10 +4,34 @@ import java.io.Serializable;
 
 public class SanPham implements Serializable {
     private String ten;
+    private String price;
+    private int amount;
     private Danhmuc danhmuc;
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
 
     public SanPham(String ten) {
         this.ten = ten;
+    }
+
+    public SanPham(String ten, String price, int amount) {
+        this.ten = ten;
+        this.price = price;
+        this.amount = amount;
     }
 
     public String getTen() {
